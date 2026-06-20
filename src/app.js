214663a14c52868938fa25d299b361f97d9a0f04
Artem Kutsan <artemkutsan@gmail.com>
2026-06-20 21:07:29 +0200

    refactor: собрал роуты через общий index.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import healthRouter from './routes/health.js';
+import apiRouter from './routes/index.js';
 import notFound from './middlewares/notFound.js';
 import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(healthRouter);
+app.use(apiRouter);
 app.use(notFound);
 app.use(errorHandler);
 
